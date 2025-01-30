acf90afd957d62cac58b45b071abda78278fb366
sachin-lakhlan <lakhlansachin0@gmail.com>
2025-01-30 16:06:25 +0530

    two section compelte

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+const navbarbtn = () => {
+    document.getElementById("name").classList.toggle("show-navbar")
+    document.getElementById("rotates").classList.toggle("rotate40")
+    document.getElementById("transparent").classList.toggle("bg-transparent")
+    document.getElementById("rotates2").classList.toggle("rotate-45")
+    document.body.classList.toggle("overflow-hidden")
+}
